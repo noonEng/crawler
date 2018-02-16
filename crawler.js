df5c4b0f6d39bc3651b980ee72f248343f490581
Akshit <akin.park@gmail.com>
2018-02-16 12:29:17 +0530

    Big bad initial commit

---
+++ crawler.js
@@ -0,0 +1,14 @@
+'use strict'
+
+/**
+ * Crawls a website using a start {url}, and returns the lexicographically smallest string.
+ * @param url
+ * @return {Promise.<string>}
+ */
+module.exports = url =>
+  new Promise((resolve, reject) => {
+    /**
+     * TODO: Write your high performance code here.
+     */
+    reject(new Error('NotImplemented'))
+  })
